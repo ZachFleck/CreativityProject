c5c1b82907eeba78e11a442f4cf26eabf778c1c7
ZachFleck <zacfleck10@gmail.com>
2018-11-07 13:13:36 -0700

    fixed error

--- CreativityProject/Controller/ArtCollectionViewController.swift
+++ CreativityProject/Controller/ArtCollectionViewController.swift
@@ -68,15 +68,15 @@ public class ArtCollectionViewController: UICollectionViewController
 
    public override func numberOfSections(in collectionView: UICollectionView) -> Int
     {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+       
+        return 1
     }
 
 
    public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+    
+        return  creativeCS.count
     }
 
     public override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
@@ -89,6 +89,18 @@ public class ArtCollectionViewController: UICollectionViewController
     }
 
     // MARK: UICollectionViewDelegate
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout colletionViewLayout: UICollectionView,
+                               sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRowCompact + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRowCompact
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
 
     /*
     // Uncomment this method to specify if the specified item should be highlighted during tracking
